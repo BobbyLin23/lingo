63072c9d36134e3272e21317e26294025cac9802
BobbyLin <linzhangsheng23@gmail.com>
2025-09-29 00:03:35 +0800

    feat: add button style

--- src/lib/components/ui/button/button.svelte
+++ src/lib/components/ui/button/button.svelte
@@ -6,23 +6,28 @@
   import { tv } from 'tailwind-variants'
 
   export const buttonVariants = tv({
-    base: 'focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive inline-flex shrink-0 items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium outline-none transition-all focus-visible:ring-[3px] disabled:pointer-events-none disabled:opacity-50 aria-disabled:pointer-events-none aria-disabled:opacity-50 [&_svg:not([class*=\'size-\'])]:size-4 [&_svg]:pointer-events-none [&_svg]:shrink-0',
+    base: 'uppercase focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive inline-flex shrink-0 items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium outline-none transition-all focus-visible:ring-[3px] disabled:pointer-events-none disabled:opacity-50 aria-disabled:pointer-events-none aria-disabled:opacity-50 [&_svg:not([class*=\'size-\'])]:size-4 [&_svg]:pointer-events-none [&_svg]:shrink-0',
     variants: {
       variant: {
-        default: 'bg-primary text-primary-foreground shadow-xs hover:bg-primary/90',
-        destructive:
-          'bg-destructive shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60 text-white',
-        outline:
-          'bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50 border',
-        secondary: 'bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80',
-        ghost: 'hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50',
-        link: 'text-primary underline-offset-4 hover:underline',
+        default: 'bg-white text-black border-slate-200 border-2 border-b-4 active:border-b-2 hover:bg-slate-100 text-slate-500',
+        primary: 'bg-sky-400 text-primary-foreground hover:bg-sky-400/90 border-sky-500 border-b-4 active:border-b-0',
+        primaryOutline: 'bg-white text-sky-500 hover:bg-slate-100',
+        secondary: 'bg-green-500 text-primary-foreground hover:bg-green-500/90 border-green-600 border-b-4 active:border-b-0',
+        secondaryOutline: 'bg-white text-green-500 hover:bg-slate-100',
+        danger: 'bg-rose-500 text-primary-foreground hover:bg-rose-500/90 border-rose-600 border-b-4 active:border-b-0',
+        dangerOutline: 'bg-white text-rose-500 hover:bg-slate-100',
+        super: 'bg-indigo-500 text-primary-foreground hover:bg-indigo-500/90 border-indigo-600 border-b-4 active:border-b-0',
+        superOutline: 'bg-white text-indigo-500 hover:bg-slate-100',
+        ghost: 'bg-transparent text-slate-500 border-transparent border-0 hover:bg-slate-100',
+        sidebar: 'bg-transparent text-slate-500 border-transparent border-2 hover:bg-slate-100 transition-none',
+        sidebarOutline: 'bg-sky-500/15 text-sky-500 border-sky-300 border-2 hover:bg-sky-500/20 transition-none',
       },
       size: {
-        default: 'h-9 px-4 py-2 has-[>svg]:px-3',
-        sm: 'h-8 gap-1.5 rounded-md px-3 has-[>svg]:px-2.5',
-        lg: 'h-10 rounded-md px-6 has-[>svg]:px-4',
-        icon: 'size-9',
+        default: 'h-11 px-4 py-2 has-[>svg]:px-3',
+        sm: 'h-9 gap-1.5 rounded-md px-3 has-[>svg]:px-2.5',
+        lg: 'h-12 rounded-md px-6 has-[>svg]:px-4',
+        icon: 'size-10',
+        rounded: 'rounded-full',
       },
     },
     defaultVariants: {
